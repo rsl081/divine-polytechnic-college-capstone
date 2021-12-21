5af3f6141dfe2db5169f8d0d7cbc309e9d363037
rsl081 <53944109+rsl081@users.noreply.github.com>
2021-12-21 14:28:40 +0800

    splash animation and connect firestore

---
+++ app/src/main/java/com/s2dioapps/divinepolytechniccollege/SplashActivity.java
@@ -0,0 +1,55 @@
+package com.s2dioapps.divinepolytechniccollege;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView ivSplash;
+    private TextView tvSplash;
+    private Animation animation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ivSplash = findViewById(R.id.splash_logo);
+        tvSplash = findViewById(R.id.title_splash);
+
+        animation = AnimationUtils.loadAnimation(this, R.anim.splash_animation);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ivSplash.startAnimation(animation);
+        tvSplash.startAnimation(animation);
+    }
+
+}
